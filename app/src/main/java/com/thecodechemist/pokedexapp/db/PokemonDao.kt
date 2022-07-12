6166f761cc79048844f90659f17a457f25f7b117
TheKemistLabs <thekemistlabs@gmail.com>
2022-07-12 20:33:22 +0100

    Added Flow wrapper to observe data changes

--- app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonDao.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonDao.kt
@@ -4,15 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PokemonDao {
 
     @Query("SELECT * FROM pokemon_table ORDER BY id ASC")
-    fun getAllPokemon(): List<Pokemon>
+    fun getAllPokemon(): Flow<List<Pokemon>>
 
     @Query("SELECT * FROM pokemon_table WHERE id = :id LIMIT 1")
-    fun getPokemonById(id: Integer): Pokemon
+    fun getPokemonById(id: Integer): Flow<Pokemon>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(pokemon: Pokemon)
