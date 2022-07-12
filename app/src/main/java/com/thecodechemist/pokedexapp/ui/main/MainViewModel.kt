e53d6c89a18c2a0ef5ef1491c3b247fb8ecac9ba
TheKemistLabs <thekemistlabs@gmail.com>
2022-07-12 22:41:52 +0100

    Added ViewModel for the Pokemon List Main Fragment

--- app/src/main/java/com/thecodechemist/pokedexapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/ui/main/MainViewModel.kt
@@ -1,7 +1,26 @@
 package com.thecodechemist.pokedexapp.ui.main
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.thecodechemist.pokedexapp.db.Pokemon
+import com.thecodechemist.pokedexapp.db.PokemonRepository
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val repository: PokemonRepository) : ViewModel() {
+
+    val allPokemon: LiveData<List<Pokemon>> = repository.allPokemon.asLiveData()
+
+    fun insert(pokemon: Pokemon) = viewModelScope.launch {
+        repository.insert(pokemon)
+    }
+}
+
+class MainViewModelFactory(private val repository: PokemonRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
