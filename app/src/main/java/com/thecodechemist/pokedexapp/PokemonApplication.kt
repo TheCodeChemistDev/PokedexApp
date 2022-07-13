31af55da234a778ad2112c56c2c2b980561c174f
TheKemistLabs <thekemistlabs@gmail.com>
2022-07-13 10:37:01 +0100

    Added Callback to PokemonRoomDatabase constructor so that data can be populated when the database is ready

--- app/src/main/java/com/thecodechemist/pokedexapp/PokemonApplication.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/PokemonApplication.kt
@@ -3,9 +3,15 @@ package com.thecodechemist.pokedexapp
 import android.app.Application
 import com.thecodechemist.pokedexapp.db.PokemonRepository
 import com.thecodechemist.pokedexapp.db.PokemonRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class PokemonApplication : Application() {
 
-    val database by lazy { PokemonRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+
+    //database and repository are only created when they are required, rather than when the app starts
+    val database by lazy { PokemonRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { PokemonRepository(database.pokemonDao())}
 }
--- app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonRoomDatabase.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonRoomDatabase.kt
@@ -4,11 +4,39 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Pokemon::class), version = 1, exportSchema = false)
 public abstract class PokemonRoomDatabase : RoomDatabase() {
 
     abstract fun pokemonDao(): PokemonDao
+
+    private class PokemonDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.pokemonDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(pokemonDao: PokemonDao) {
+            //Delete the current data from the database
+            pokemonDao.deleteAll()
+
+            //TODO: Pull new data from API using Retrofit and re-populate the database
+            // ...
+        }
+
+    }
+
+
 
     //Singleton Database to prevent multiple instances
     companion object {
@@ -16,14 +44,17 @@ public abstract class PokemonRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: PokemonRoomDatabase? = null
 
-        fun getDatabase(context: Context): PokemonRoomDatabase {
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): PokemonRoomDatabase {
             //Create the database if it does not already exist
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     PokemonRoomDatabase::class.java,
                     "pokemon_database"
-                ).build()
+                ).addCallback(PokemonDatabaseCallback(scope)).build()
                 INSTANCE = instance
 
                 instance
