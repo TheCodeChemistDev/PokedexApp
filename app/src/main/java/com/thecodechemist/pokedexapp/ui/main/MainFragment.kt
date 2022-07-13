3f4c88694ae26d325229922ccd99401130d170f8
TheKemistLabs <thekemistlabs@gmail.com>
2022-07-13 10:16:54 +0100

    Set Pokemon adapter on RecyclerView

--- app/src/main/java/com/thecodechemist/pokedexapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/ui/main/MainFragment.kt
@@ -1,13 +1,16 @@
 package com.thecodechemist.pokedexapp.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.thecodechemist.pokedexapp.R
+import com.thecodechemist.pokedexapp.adapter.PokemonAdapter
 import com.thecodechemist.pokedexapp.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
@@ -28,6 +31,14 @@ class MainFragment : Fragment() {
     ): View {
         _binding = MainFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = binding.recyclerView
+        val adapter = PokemonAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
     }
 
     override fun onDestroyView() {
