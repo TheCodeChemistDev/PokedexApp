7d2e9d70c2599e268f9e671f56464197bc557e17
TheKemistLabs <thekemistlabs@gmail.com>
2022-07-13 12:04:11 +0100

    Added loop to pull each Pokemon's data from the API and add Insert into the Room Database

--- app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonRoomDatabase.kt
+++ app/src/main/java/com/thecodechemist/pokedexapp/db/PokemonRoomDatabase.kt
@@ -1,12 +1,18 @@
 package com.thecodechemist.pokedexapp.db
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.thecodechemist.pokedexapp.network.PokemonApi
+import com.thecodechemist.pokedexapp.network.PokemonApiService
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONObject
+import org.json.JSONTokener
 
 @Database(entities = arrayOf(Pokemon::class), version = 1, exportSchema = false)
 public abstract class PokemonRoomDatabase : RoomDatabase() {
@@ -30,8 +36,19 @@ public abstract class PokemonRoomDatabase : RoomDatabase() {
             //Delete the current data from the database
             pokemonDao.deleteAll()
 
-            //TODO: Pull new data from API using Retrofit and re-populate the database
-            // ...
+            for(i in 1..20) {
+                try {
+                    val apiResponse = PokemonApi.retrofitService.getPokemonById(i.toString())
+                    val jsonResultsObject = JSONTokener(apiResponse).nextValue() as JSONObject
+                    val pokemonName = jsonResultsObject.getString("name")
+                    val pokemonSpriteUrlsObject = jsonResultsObject.getJSONObject("sprites")
+                    val pokemonSpriteUrl = pokemonSpriteUrlsObject.getString("front_default")
+                    val newPokemon = Pokemon(i as Integer, pokemonName, pokemonSpriteUrl)
+                    pokemonDao.insert(newPokemon)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
 
     }
@@ -48,6 +65,7 @@ public abstract class PokemonRoomDatabase : RoomDatabase() {
             context: Context,
             scope: CoroutineScope
         ): PokemonRoomDatabase {
+            Log.i("Database", "Getting Database")
             //Create the database if it does not already exist
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
